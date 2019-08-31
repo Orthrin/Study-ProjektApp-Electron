16a91dff1d648ea5e845fd6eb83dbc26b163a074
orthrin <gronzelg@gmail.com>
2019-09-01 02:51:58 +0300

    Borderless Window

---
+++ app.js
@@ -0,0 +1,49 @@
+// # ==== Precursor Directives ====
+//
+const electron = require('electron');
+const { app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// # ==== Variables ====
+//
+var imgDirectory = '/images/';
+var appDirectory = '/application/';
+
+// # ==== Functions ====
+//
+// this function creates a variable for window size
+function createWindow()
+{
+    //define a variable for window
+    let win = new  BrowserWindow({
+        width: 1000,
+        height: 600,
+        icon: path.join(__dirname, imgDirectory, 'mindsculpt.png'),
+        frame: false,
+        webPreferences:{
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, appDirectory, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Open the DevTools.
+    //win.webContents.openDevTools()
+}
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    // controls if its a mac
+    if(process.platform !== 'darwin' )
+    {
+        app.quit();
+    }
+});
