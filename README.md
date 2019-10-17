433a712614fb8894528245be4036da6309f9b074
orthrin <gronzelg@gmail.com>
2019-10-18 02:57:17 +0300

    tray, context menu test

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ProjektApp-Electron
  Text, Visual, Node Based Note Taking / External Memory App
+ Created at 2019-Oct-17
+
+## Console Commands
+ ```
+npm install --save-dev electron
+npm install jquery --save
+```
+
+## ToDo
+Tray Application
+Menus
+Search Bar
+Change Modules
--- app.js
+++ app.js
@@ -1,41 +1,54 @@
-// # ==== Libraries & Notes ====
-//
-// npm install --save-dev electron
-// npm install jquery --save
-
-// # ==== Precursor Directives ====
+// # Precursor Directives
 //
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain, Menu } = require('electron'); // ipc nin ne olduguna bak
-const defaultProps = {
-    width: 500,
-    height: 800,
-    show: false
-}
-//const path = require('path');
+const { app, BrowserWindow, Tray, ipcMain, Menu } = require('electron'); // ipc nin ne olduguna bak
+
+const path = require('path');
+const url = require('url');
+const defaultProps = { width: 500, height: 800, show: false };
 // const { menuTemplate } = require('./application/js/menu.js');
 // const menu = Menu.buildFromTemplate(menuTemplate);
 
-// # ==== Variables ====
+// # Variables
 //
-var iconPath = '/application/resources/img/mindsculpt.png';
+var iconPath = 'application/resources/img/mindsculpt.png';
+var iconPath2 = 'application/resources/img/logo.png';
+let appIcon = null;
+let win = null;
 
-// # ==== Functions ====
+// # Tray Menus
+//
+let contextMenu = Menu.buildFromTemplate([
+    {
+        label: 'item1',
+        type: 'radio'
+    },
+    {
+        label: 'item2',
+        submenu: [
+            { label: 'submenu1'},
+            { label: 'submenu2'}
+        ]
+    }
+]);
+
+
+// # Functions
 //
 // this function creates a variable for window size
 function createWindow()
 {
     //define a variable for window
-    let mainWindow = new  BrowserWindow({
+    let mainWindow = new BrowserWindow({
         width: 1024,
         height: 720,
         minWidth: 180, // set a min width!
         minHeight: 120, // and a min height!
         icon: __dirname + iconPath,
         backgroundColor: '#FFF',
-        frame: false, 
+        frame: false,
         titleBarStyle: 'hidden',
-        webPreferences:{
+        webPreferences: {
             nodeIntegration: true
         }
     });
@@ -44,18 +57,23 @@ function createWindow()
     mainWindow.loadURL(`file://${__dirname}/application/html/index.html`);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools({detached:true});
+    mainWindow.webContents.openDevTools({ detached: true });
 }
 
 
-app.on('ready', () =>{
+app.on('ready', () =>
+{
     // Menu.setApplicationMenu(menu);
     createWindow();
+    appIcon = new Tray(iconPath2);
+    appIcon.setToolTip('Electron Application Test Tray');
+    appIcon.setContextMenu(contextMenu);
 });
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', () =>
+{
     // controls if its a mac
-    if(process.platform !== 'darwin' )
+    if (process.platform !== 'darwin')
     {
         app.quit();
     }
